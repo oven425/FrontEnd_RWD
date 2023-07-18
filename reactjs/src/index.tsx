2c63bbb163a053e5d48e6986b91ea8dce90cdedd
oven425 <oven425@yahoo.com.tw>
2023-07-18 10:01:15 +0800

    update 1. study rwd and css

--- reactjs/src/index.tsx
+++ reactjs/src/index.tsx
@@ -3,13 +3,37 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Root from "./routes/root";
+import ErrorPage from "./error-page";
+import Contact from "./routes/contact";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root/>,
+    errorElement:<ErrorPage/>,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+      },
+    ],
+  },
+  // {
+  //   path: "contacts/:contactId",
+  //   element: <Contact />,
+  // },
+]);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
