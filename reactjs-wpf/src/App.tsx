b79a6f16e1ad4348ae5a76b15391feb0acb96252
oven425 <oven425@yahoo.com.tw>
2023-10-17 10:31:27 +0800

    update 1. test new object in tsx

--- reactjs-wpf/src/App.tsx
+++ reactjs-wpf/src/App.tsx
@@ -1,15 +1,19 @@
-import React, { ReactElement, useRef } from 'react';
+import React, { ReactElement, useRef, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import ChildWindow, { IChildWindowRef } from './ChildWindow';
 import { Window } from './Window';
-import { Border, SoildBrush } from './Border';
+import { Border, SoildBrush, VerticalAlignment } from './Border';
+import { writeFile } from 'fs';
 
 
 function App() {
+  const [width, setWidth] = useState(100)
   const bn = useRef(null)
   const cw = useRef<IChildWindowRef>(null);
-  const back=useRef<SoildBrush>(new SoildBrush());
+  const back=useRef<SoildBrush>(new SoildBrush(0,0,255));
+
+
   return (
     // <Window>
     //   <TextBlock text='hellow world!' VerticalAlignment={VerticalAlignment.Stretch}/>
@@ -19,8 +23,15 @@ function App() {
       {/* <p className='self-center justify-self-center'>hellow world!</p> */}
       {/* <div className=' self-stretch justify-self-stretch bg-green-400'></div> */}
       {/* <div className=' w-10 h-10 self-center justify-self-center bg-green-400'></div> */}
-      <Border background={back.current}/>
+      <Border background={new SoildBrush(255,0,0)} VerticalAlignment={VerticalAlignment.Center} width={width}/>
+      
+      <input type='range' max={500} min={0} value={width} onChange={e=>setWidth(parseInt(e.target.value))}></input>
+      {width}
     </div>
+    
+    // <div>
+    //   <button onClick={()=>aa()}>write</button>
+    // </div>
   );
 }
 
--- reactjs-wpf/src/Border.tsx
+++ reactjs-wpf/src/Border.tsx
@@ -1,27 +1,42 @@
-import { CSSProperties } from "react"
+import { CSSProperties, useEffect, useState } from "react"
 export enum VerticalAlignment {
     Top,
     Center,
     Bottom,
     Stretch
 }
+
+export const VerticalAlignmentToCSS = (data:VerticalAlignment):CSSProperties=>{
+    return({
+        alignSelf:'center'
+    })
+}
 interface IBrush{
     ToCSS(): CSSProperties;
 }
 
 export class SoildBrush implements  IBrush{
-    SoildBrush(r:number){
-
-    }
-    
+    constructor(r:number, g:number, b:number, a:number=255) {
+        this.r = r
+        this.g = g
+        this.b = b
+        this.a = a
+      }
+    r:number = 0;
+    g:number = 0;
+    b:number = 0;
+    a:number = 0;
     ToCSS(): CSSProperties {
         return ({
-            width:"50px",
-            height:"50px",
-            backgroundColor:'rgb(255,255,0)'
+            backgroundColor:`rgb(${this.r},${this.g},${this.b})`
           });
     }
 
+}
+
+class UIProps
+{
+    width:number = 0;
 }
 
 type BorderProps={
@@ -31,7 +46,16 @@ type BorderProps={
     height?:number
 }
 export const Border=(props:BorderProps)=>{
-    
+    useEffect(()=>{
+         console.log(`propschange: ${props.width}`);
+         let ww = props.width;
+         
+         setStyle({
+            ... props.background?.ToCSS(),
+            width:`${props.width}px`,
+            height:'50px',
+         })
+    },[props])
     const ver=():CSSProperties=>{
         return ({
             alignSelf:'center'
@@ -42,8 +66,9 @@ export const Border=(props:BorderProps)=>{
         ...props.background?.ToCSS(),
         ...ver()
     }
+    const [style, setStyle] = useState<CSSProperties>();
     return(
-        <div style={{...props.background?.ToCSS(),...ver()}}>
+        <div style={style}>
 
         </div>
     )
